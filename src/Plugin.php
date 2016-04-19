034041771628be8f7e77907bf8b605b0ef1dd2cc
Alan Storm <astorm@alanstorm.com>
2016-04-19 14:12:38 -0700

    initial real commit

---
+++ src/Plugin.php
@@ -0,0 +1,23 @@
+<?php
+namespace Pulsestorm\Composer\Example;
+class Plugin implements PluginInterface, EventSubscriberInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        file_put_contents('/tmp/composer.log', __METHOD__ . "\n",FILE_APPEND);
+    }
+    
+    public static function getSubscribedEvents()
+    {
+        return array(
+            'post-install-cmd' => 'installOrUpdate',
+            'post-update-cmd' => 'installOrUpdate',            
+        );
+    }    
+    
+    public function installOrUpdate($event)
+    {
+        file_put_contents('/tmp/composer.log', __METHOD__ . "\n",FILE_APPEND);
+        file_put_contents('/tmp/composer.log', get_class($event) . "\n",FILE_APPEND);            
+    }
+}
